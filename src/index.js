0520a9c59919169ebdf7d898cb81630d0ebf42c4
Elmer <elmer@icetech.com.tw>
2025-07-14 15:25:14 +0800

    test publish

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Button from '../stories/Button.vue'
+import Header from '../stories/Header.vue'
+import Page from '../stories/Page.vue'
+
+export {Button, Header, Page}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 如有使用 @ 別名
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'DemoUiLib',
+      fileName: 'demo-ui-lib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 })
